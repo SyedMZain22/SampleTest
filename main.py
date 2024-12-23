aef4a9ab1b806e5416be695b28ac9864d37ee060
Syed Zain <143264837+SyedMZain22@users.noreply.github.com>
2024-12-23 14:20:53 +0500

    Update main.py
    new ones

--- main.py
+++ main.py
@@ -10,6 +10,11 @@ def generate_random_password(length=12):
     
     return password
 
+# Generate a random password and save it to a file
+random_password = generate_random_password()
+
+# Save the random password to a file
 with open('password_output.txt', 'w') as f:
     f.write(f"Generated Random Password: {random_password}")
 
+print(f"Generated Random Password: {random_password}")
